0a9ac10569d8de718f49849f6f92a948978a2bac
Christian Haddrell <jodre11.public@gmail.com>
2024-02-04 15:15:36 +0000

    Initial simple HelloFrom Implementation

---
+++ Test/Jodre11.EnumerateImplementations.Test.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Jodre11.EnumerateImplementations.Test.Console;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+
+    static partial void HelloFrom(string name);
+}
